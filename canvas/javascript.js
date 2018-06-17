fd0a7e92a71f84e4eafe23f3a5f18c46f5471804
Sophia <phuongmai001@gmail.com>
2018-06-17 16:06:11 -0700

    update game file

--- canvas/javascript.js
+++ canvas/javascript.js
@@ -1,8 +1,26 @@
-window.onload = function() {
-  gameBasic();
+window.onload = function() { startGame(); }
+
+function startGame() {
+  myGameArea.start();
+  const myGamePiece = new component(30, 30, 'red', 10, 120);
 }
 
-function gameBasic() {
-  const myGame = document.getElementById("myGame");
-  const context = clock.getContext('2d');
+const myGameArea = {
+  canvas: document.createElement('canvas'),
+  start: function() {
+    this.canvas.width = 500; // overridden by style tag
+    this.canvas.height = 300; // overridden by style tag
+    this.context = this.canvas.getContext('2d');
+    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+  },
 };
+
+function component(width, height, color, x, y) {
+  this.width = width;
+  this.height = height;
+  this.x = x;
+  this.y = y;    
+  ctx = myGameArea.context;
+  ctx.fillStyle = color;
+  ctx.fillRect(this.x, this.y, this.width, this.height);
+}
