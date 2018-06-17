73570885bf586a94de3d53ec8cc03567dc812dff
Sophia <phuongmai001@gmail.com>
2018-06-17 00:20:09 -0700

    add flightpath

--- googlemap/googlemaps.js
+++ googlemap/googlemaps.js
@@ -1,10 +1,12 @@
 function myMap() {
   const myCenter = new google.maps.LatLng(37.335942, -121.886908);
-  const bigBasin = new google.maps.LatLng(37.172670, -121.221556);
-  const santaCruz = new google.maps.LatLng(36.966560, -122.016581)
+  const bigBasin = new google.maps.LatLng(37.172670, -122.221556);
+  const santaCruz = new google.maps.LatLng(36.966560, -122.016581);
+
+  const myTrip = [myCenter, bigBasin, santaCruz];
 
   const mapCanvas = document.getElementById("googleMap");
-  const mapOptions = {center:myCenter, zoom:10};
+  const mapOptions = {center:myCenter, zoom:9};
 
   const map = new google.maps.Map(mapCanvas, mapOptions);
 
@@ -13,6 +15,14 @@ function myMap() {
     animation: google.maps.Animation.BOUNCE
   });
   marker.setMap(map);
+
+  const flightPath = new google.maps.Polyline({
+    path: myTrip,
+    strokeColor: "#0000FF",
+    strokeOpacity: 0.8,
+    strokeWeight: 2
+  });
+  flightPath.setMap(map);
 }
 
 
