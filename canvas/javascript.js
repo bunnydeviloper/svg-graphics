d4d41cd11209cf66e5b9c66e619806cb18621ea0
Sophia <phuongmai001@gmail.com>
2018-06-17 19:46:15 -0700

    update game

--- canvas/javascript.js
+++ canvas/javascript.js
@@ -15,6 +15,13 @@ const myGameArea = {
     this.context = this.canvas.getContext('2d');
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     this.interval = setInterval(updateGameArea, 20);
+    window.addEventListener('keydown', function(e) {
+        console.log(e.keyCode);
+      myGameArea.key = e.keyCode;
+    })
+    window.addEventListener('keyup', function(e) {
+      myGameArea.key = false;
+    })
   },
   clear: function() {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
