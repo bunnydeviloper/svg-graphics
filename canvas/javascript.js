15914c5bab35f1557b3f032e566c0fd9f0629543
ThanhPhuong Mai <MaiAwesomeMac@thanhphuongsmbp.attlocal.net>
2018-06-17 22:57:14 -0700

    update game

--- canvas/javascript.js
+++ canvas/javascript.js
@@ -4,23 +4,24 @@ let myGamePiece;
 
 function startGame() {
   myGameArea.start();
-  myGamePiece = new component(30, 30, 'blue', 10, 120);
+  myGamePiece = new component(20, 20, 'blue', 10, 70);
 }
 
 const myGameArea = {
   canvas: document.createElement('canvas'),
   start: function() {
-    this.canvas.width = 500; // overridden by style tag
-    this.canvas.height = 300; // overridden by style tag
+    // this.canvas.width = 500; // override the style tag
+    // this.canvas.height = 300; // override the style tag
     this.context = this.canvas.getContext('2d');
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     this.interval = setInterval(updateGameArea, 20);
     window.addEventListener('keydown', function(e) {
-        console.log(e.keyCode);
-      myGameArea.key = e.keyCode;
+      myGameArea.keys = (myGameArea.keys || []);
+      console.log(e.keyCode);
+      myGameArea.keys[e.keyCode] = (e.type == "keydown"); // true
     })
     window.addEventListener('keyup', function(e) {
-      myGameArea.key = false;
+      myGameArea.keys[e.keyCode] = (e.type == "keydown") // false
     })
   },
   clear: function() {
@@ -48,6 +49,10 @@ function component(width, height, color, x, y) {
 
 function updateGameArea() {
   myGameArea.clear();
+  if (myGameArea.keys && myGameArea.keys[37]) myGamePiece.speedX = -1;
+  if (myGameArea.keys && myGameArea.keys[39]) myGamePiece.speedX = +1;
+  if (myGameArea.keys && myGameArea.keys[38]) myGamePiece.speedY = -1;
+  if (myGameArea.keys && myGameArea.keys[40]) myGamePiece.speedY = +1;
   myGamePiece.newPos();
   myGamePiece.updateRect();
 }
